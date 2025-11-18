0f0d7785b60a177a742d35d3887d74d1fa7dcbae
Z Lin <zlin399@wisc.edu>
2025-11-17 22:23:14 -0600

    add arco

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
+import App from './App.tsx'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root') as HTMLElement;
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
